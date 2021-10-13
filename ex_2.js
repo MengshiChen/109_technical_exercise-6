8cedf28abca5fbd9bbe51bdfd95949a2bcb3ab13
MengshiChen <moussechen0205@gmail.com>
2021-10-13 13:00:11 -0700

    chessboard

--- ex_2.js
+++ ex_2.js
@@ -1,9 +1,11 @@
-let print="";
-for (let number = 1; number <=100; numer = number+1;){
-  if (number%3 ==0); print=print+"Fizz";
-  if(number%5 ==0 ); print=print+"Buzz";
-  console.log(print||number);
-
-  else (number%3 ==0; number%5 ==0); print= print+"FizzBuzz";
-  console.log(print||number);
+for (let number = 1; number <= 100; number=number+1) {
+  let print = "";
+  if (number % 3 == 0) print= print + "Fizz";
+  if (number % 5 == 0) print= print + "Buzz";
+  else if (number %3 == 0 && number %5 == 0) print= print +"FizzBuzz";
+  console.log(print || number);
 }
+//
+//   else (number%3 ==0; number%5 ==0); print= print+"FizzBuzz";
+//   console.log(print||number);
+// }
